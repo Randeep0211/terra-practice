d21748da48150046ad5715c95f6ac87ec7041be7
Randeep singh <jazzysingh227@gmail.com>
2025-05-18 17:28:12 +0530

    folder changes

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
-  region  = var.aws_region
+  region  = "ap-south-1"
+}
+
+module "ec2_instance" {
+  source = "./modules/ec2"
+  aws_region = "ap-south-1"
+  aws_ami = "ami-0e35ddab05955cf57"
+  aws_instance_type = "t2.micro"
+  aws_instance_tag = "terra-abhi"
 }
--- terraform.tfvars
+++
@@ -1,4 +0,0 @@
-aws_region = "ap-south-1"
-aws_ami = "ami-0e35ddab05955cf57"
-aws_instance_type = "t2.micro"
-aws_instance_tag = "terra-gagan"
