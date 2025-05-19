20d7c1c2fbb28af6e0f67f9d32195f6d92cd1229
Randeep singh <jazzysingh227@gmail.com>
2025-05-19 19:55:19 +0530

    change

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region  = "ap-south-1"
 }
 
+# module "ec2_instance" {
+#   source = "./modules/ec2"
+#   aws_region = "ap-south-1"
+#   aws_ami = "ami-0e35ddab05955cf57"
+#   aws_instance_type = "t2.micro"
+#   aws_instance_tag = "terra-demo"
+# }
+
 module "ec2_instance" {
   source = "./modules/ec2"
   aws_region = "ap-south-1"
