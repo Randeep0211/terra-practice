2142ffbb1757401e0e2137e2a95d0b9e50d022d1
Randeep singh <jazzysingh227@gmail.com>
2025-05-19 20:43:06 +0530

    backend encryption

--- backend.tf
+++ backend.tf
@@ -1,7 +1,11 @@
+// Remote Backend concept
+
+
 terraform{
   backend "s3" {
     bucket = "classified-file"
     key    = "terra/terraform.tfstate"
     region = "us-east-2"
+    encrypt = true
   }
 }
